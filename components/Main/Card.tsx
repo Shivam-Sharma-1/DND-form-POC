6625ebbda8c84568d68fd333435672325973b841
Shivam77607 <shivamsharma77607@gmail.com>
2024-11-13 18:37:13 +0530

    feat: add unique ID prop to Card component for improved drag-and-drop support

--- components/Main/Card.tsx
+++ components/Main/Card.tsx
@@ -1,6 +1,9 @@
-const Card = ({ title }: { title: string }) => {
+const Card = ({ title, id }: { title: string; id: string }) => {
   return (
-    <div className="flex justify-center items-center bg-blue-400 border border-blue-800 rounded-md py-2">
+    <div
+      id={id}
+      className="flex justify-center items-center bg-blue-400 border border-blue-800 rounded-md py-2"
+    >
       {title}
     </div>
   );

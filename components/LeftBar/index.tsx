089f2d7482d95383a07efb40914acb631f1fe2dd
Shivam77607 <shivamsharma77607@gmail.com>
2024-11-13 18:37:08 +0530

    feat: update LeftBar component to support dragging with unique card IDs

--- components/LeftBar/index.tsx
+++ components/LeftBar/index.tsx
@@ -1,12 +1,22 @@
-import React from "react";
 import Card from "./Card";
+import { v4 as uuidv4 } from "uuid";
 
 const cards = [{ title: "Card 1" }, { title: "Card 2" }, { title: "Card 3" }];
-const LeftBar = () => {
+
+const LeftBar = ({
+  handleDragging,
+}: {
+  handleDragging: (dragging: boolean) => void;
+}) => {
   return (
     <div className="max-w-[20%] bg-slate-400 w-full flex flex-col gap-1 py-4 px-2">
       {cards.map((card) => (
-        <Card key={card.title} title={card.title} />
+        <Card
+          key={card.title}
+          id={uuidv4()}
+          title={card.title}
+          handleDragging={handleDragging}
+        />
       ))}
     </div>
   );

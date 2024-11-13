0d20cef92d6d2d6308d6057024cbee7df8b9e461
Shivam77607 <shivamsharma77607@gmail.com>
2024-11-13 18:36:57 +0530

    feat: integrate LeftBar and drag-and-drop functionality into Home layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
+"use client";
+
+import LeftBar from "@/components/LeftBar";
 import Main from "@/components/Main";
+import { Data, useDragAndDrop } from "@/hooks/useDragDrop";
+
+const cards: Data[] = [{ title: "Dummy", id: "wefwe" }];
 
 export default function Home() {
+  const { listItems, handleUpdateList, handleDragging } = useDragAndDrop(cards);
+
   return (
-    <div className="flex-1 bg-slate-600">
-      <Main />
-    </div>
+    <main className="flex w-full min-h-screen">
+      <LeftBar handleDragging={handleDragging} />
+      <div className="flex-1 bg-slate-600">
+        <Main
+          listItems={listItems}
+          handleUpdateList={handleUpdateList}
+          handleDragging={handleDragging}
+        />
+      </div>
+    </main>
   );
 }

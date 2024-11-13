eb7899ca4e6e59722430114ead8a1aafb944e001
Shivam77607 <shivamsharma77607@gmail.com>
2024-11-13 19:04:40 +0530

    feat: extend Card component to handle drop events and update list with new props

--- components/Main/Card.tsx
+++ components/Main/Card.tsx
@@ -1,7 +1,32 @@
-const Card = ({ title, id }: { title: string; id: string }) => {
+interface Props {
+  title: string;
+  id: string;
+  handleUpdateList: (id: string, title: string, position?: number) => void;
+  handleDragging: (dragging: boolean) => void;
+  position: number;
+}
+
+const Card = ({
+  title,
+  id,
+  handleDragging,
+  handleUpdateList,
+  position,
+}: Props) => {
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    handleUpdateList(
+      e.dataTransfer.getData("id"),
+      e.dataTransfer.getData("title"),
+      position
+    );
+    handleDragging(false);
+  };
+
   return (
     <div
       id={id}
+      onDrop={handleDrop}
       className="flex justify-center items-center bg-blue-400 border border-blue-800 rounded-md py-2"
     >
       {title}

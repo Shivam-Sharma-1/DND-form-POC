cc78be7ebdbc54a9ce10cfd5cda10a4d16a809b3
Shivam77607 <shivamsharma77607@gmail.com>
2024-11-13 20:52:47 +0530

    feat: add drag-and-drop event handlers for improved interactivity

--- components/Main/Card.tsx
+++ components/Main/Card.tsx
@@ -24,10 +24,20 @@ const Card = ({
     handleDragging(false);
   };
 
+  const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+    e.dataTransfer.setData("id", `${id}`);
+    e.dataTransfer.setData("title", `${title}`);
+    handleDragging(true);
+  };
+  const handleDragEnd = () => handleDragging(false);
+
   return (
     <div
       id={id}
       onDrop={handleDrop}
+      draggable
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
       className="flex justify-center items-center bg-blue-400 border border-blue-800 rounded-md py-2"
     >
       {title}

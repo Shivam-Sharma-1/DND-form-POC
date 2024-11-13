8942862ac33e97b1c7e4f3e4244709391c348dfc
Shivam77607 <shivamsharma77607@gmail.com>
2024-11-13 17:38:05 +0530

    feat: integrate Main component into Home layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Main from "@/components/Main";
+
 export default function Home() {
-  return <div className="flex-1 bg-slate-600">Hello, World!</div>;
+  return (
+    <div className="flex-1 bg-slate-600">
+      <Main />
+    </div>
+  );
 }

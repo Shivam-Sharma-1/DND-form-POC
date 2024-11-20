45d25587e29951d6b0fe557fdee64082c1d1fe3a
Shivam77607 <shivamsharma77607@gmail.com>
2024-11-20 09:50:17 +0530

    refactor: update to use TemplateProps and render input based on type

--- components/Main/MainCard.tsx
+++ components/Main/MainCard.tsx
@@ -1,13 +1,34 @@
+import { InputType, TemplateProps } from "@/types";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
+import { Label } from "../UI/shadcn/label";
+import { Input } from "../UI/shadcn/input";
 
-const MainCard = ({ id, title }: { id: string; title: string }) => {
+const MainCard = ({ inputData }: { inputData: TemplateProps }) => {
   const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id: id });
+    useSortable({ id: inputData.id });
+
+  const inputCard =
+    inputData.type === InputType.TEXT ? (
+      <div className="flex flex-col justify-center gap-2">
+        <Label htmlFor="answer" className="font-medium">
+          Answer
+        </Label>
+        <Input
+          id="answer"
+          type="text"
+          placeholder="Enter the answer"
+          className="rounded-md border border-green-800 p-2"
+          maxLength={Number(inputData.maxLength)}
+        />
+      </div>
+    ) : (
+      ""
+    );
 
   return (
     <div
-      id={id}
+      id={inputData.id}
       ref={setNodeRef}
       {...attributes}
       {...listeners}
@@ -17,7 +38,10 @@ const MainCard = ({ id, title }: { id: string; title: string }) => {
       }}
       className="flex justify-center items-center bg-blue-400 border border-blue-800 rounded-md py-2 gap-16"
     >
-      <p>{title}</p>
+      <div>
+        <p>{inputData.question}</p>
+        {inputCard}
+      </div>
     </div>
   );
 };

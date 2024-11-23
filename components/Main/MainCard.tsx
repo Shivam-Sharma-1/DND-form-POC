3d1af5f87f25bf323a9011c8ee19ac2fa69d9e90
Shivam77607 <shivamsharma77607@gmail.com>
2024-11-23 20:22:34 +0530

    refactor: enhance MainCard to support dynamic MCQ options and improve input handling

--- components/Main/MainCard.tsx
+++ components/Main/MainCard.tsx
@@ -8,6 +8,7 @@ import { Input } from "../UI/shadcn/input";
 import { Dispatch, SetStateAction, useRef, useState } from "react";
 import { Button } from "../UI/shadcn/button";
 import { Ellipsis, Trash2 } from "lucide-react";
+import { cn } from "@/utils/helpers/styles";
 
 const MainCard = ({
   inputData,
@@ -21,15 +22,20 @@ const MainCard = ({
     useSortable({ id: inputData.id, disabled: isDragging });
   const [question, setQuestion] = useState(inputData.question);
   const [minLength, setMinLength] = useState(
-    inputData.type === "text" ? inputData.minLength : "0"
+    inputData.type === InputType.TEXT ? inputData.minLength : "0"
   );
   const [maxLength, setMaxLength] = useState(
-    inputData.type === "text" ? inputData.maxLength : "0"
+    inputData.type === InputType.TEXT ? inputData.maxLength : "0"
   );
+  const [options, setOptions] = useState(
+    inputData.type === InputType.MCQ ? inputData.options : []
+  );
+  // const [optionCount, setOptionCount] = useState(
+  //   inputData.type === InputType.MCQ ? inputData.options.length : 0
+  // );
   const dragOverlayRef = useRef<HTMLDivElement>(null);
 
   const handleDelete = () => {
-    console.log("deleted");
     setInputsList((prevList) =>
       prevList.filter((input) => input.id !== inputData.id)
     );
@@ -38,38 +44,82 @@ const MainCard = ({
   const handleFocus = () => setIsDragging(true);
   const handleBlur = () => setIsDragging(false);
 
-  const inputCard = inputData.type === InputType.TEXT && (
-    <div className="flex items-center gap-2 w-full">
-      <div className="flex flex-col justify-center gap-2 w-full flex-1">
-        <Label htmlFor={`minlength-${inputData.id}`} className="font-medium">
-          Min Length
-        </Label>
-        <Input
-          id={`minlength-${inputData.id}`}
-          type="number"
-          placeholder="Enter the Minimum length of the answer"
-          value={minLength}
-          onChange={(e) => setMinLength(e.target.value)}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-        />
+  const TextInputs = () => {
+    return (
+      <div className="flex items-center gap-2 w-full">
+        <div className="flex flex-col justify-center gap-2 w-full flex-1">
+          <Label htmlFor={`minlength-${inputData.id}`} className="font-medium">
+            Min Length
+          </Label>
+          <Input
+            id={`minlength-${inputData.id}`}
+            type="number"
+            placeholder="Enter the Minimum length of the answer"
+            value={minLength}
+            onChange={(e) => setMinLength(e.target.value)}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
+        </div>
+        <div className="flex flex-col justify-center gap-2 w-full flex-1">
+          <Label htmlFor={`maxlength-${inputData.id}`} className="font-medium">
+            Max Length
+          </Label>
+          <Input
+            id={`maxlength-${inputData.id}`}
+            type="number"
+            placeholder="Enter the Maximum length of the answer"
+            value={maxLength}
+            onChange={(e) => setMaxLength(e.target.value)}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
+        </div>
       </div>
-      <div className="flex flex-col justify-center gap-2 w-full flex-1">
-        <Label htmlFor={`maxlength-${inputData.id}`} className="font-medium">
-          Max Length
-        </Label>
-        <Input
-          id={`maxlength-${inputData.id}`}
-          type="number"
-          placeholder="Enter the Maximum length of the answer"
-          value={maxLength}
-          onChange={(e) => setMaxLength(e.target.value)}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-        />
+    );
+  };
+
+  const MCQInputs = () => {
+    return (
+      <div className="flex items-center gap-2 w-full">
+        <div className="flex flex-col justify-center gap-2 w-full">
+          {options.map((option, index) => (
+            <div key={index} className="flex items-center gap-4 w-full">
+              <Label
+                htmlFor={`option-${index}-${inputData.id}`}
+                className="font-medium shrink-0 min-w-16"
+              >
+                Option {index + 1}
+              </Label>
+              <Input
+                id={`option-${index}-${inputData.id}`}
+                type="text"
+                placeholder="Enter the option"
+                value={option}
+                className="flex-1"
+                onChange={(e) =>
+                  setOptions((prevOptions) =>
+                    prevOptions.map((opt, i) =>
+                      i === index ? e.target.value : opt
+                    )
+                  )
+                }
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+              />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
+
+  const inputCard =
+    inputData.type === InputType.TEXT ? (
+      <TextInputs />
+    ) : inputData.type === InputType.MCQ ? (
+      <MCQInputs />
+    ) : null;
 
   return (
     <div
@@ -78,7 +128,10 @@ const MainCard = ({
         transform: CSS.Transform.toString(transform),
         transition,
       }}
-      className={`flex items-center bg-white border border-border rounded-md py-6 px-6 gap-16 shadow-md group relative`}
+      className={cn(
+        `flex items-center bg-white border border-border rounded-md py-6 px-6 gap-16 shadow-md group relative`,
+        isDragging && "z-20"
+      )}
     >
       <div
         {...attributes}

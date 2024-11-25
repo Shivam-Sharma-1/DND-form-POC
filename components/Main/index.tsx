3afe57a90f0d915dacba52fcd3eea0a13a7c9aca
Shivam77607 <shivamsharma77607@gmail.com>
2024-11-25 22:33:47 +0530

    refactor: comment out unused drag-and-drop logic and clean up component structure

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -1,12 +1,12 @@
 import {
-  arrayMove,
   SortableContext,
   verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
-import { DndContext, DragEndEvent, useDroppable } from "@dnd-kit/core";
+import { useDroppable } from "@dnd-kit/core";
 import { Dispatch, SetStateAction } from "react";
 import { TemplateProps } from "@/types";
 import MainCard from "./MainCard";
+import { cn } from "@/utils/helpers/styles";
 
 export default function Main({
   inputsList,
@@ -17,40 +17,43 @@ export default function Main({
 }) {
   const { isOver, setNodeRef } = useDroppable({ id: "droppable" });
 
-  const reorderInputsList = (e: DragEndEvent) => {
-    if (!e.over) return;
+  // const reorderInputsList = (e: DragEndEvent) => {
+  //   if (!e.over) return;
 
-    setInputsList((prevInputsList) => {
-      const oldIndex = prevInputsList.findIndex(
-        (input) => input.id === e.active.id
-      );
-      const newIndex = prevInputsList.findIndex(
-        (input) => input.id === e.over!.id
-      );
-      return arrayMove(prevInputsList, oldIndex, newIndex);
-    });
-  };
+  //   setInputsList((prevInputsList) => {
+  //     const oldIndex = prevInputsList.findIndex(
+  //       (input) => input.id === e.active.id
+  //     );
+  //     const newIndex = prevInputsList.findIndex(
+  //       (input) => input.id === e.over!.id
+  //     );
+  //     return arrayMove(prevInputsList, oldIndex, newIndex);
+  //   });
+  // };
 
   return (
     <div
-      className="w-full h-full flex flex-col gap-4 py-4 px-20 items-center"
+      className={cn(
+        "w-full h-full flex flex-col gap-4 py-4 px-20 items-center",
+        isOver ? "" : ""
+      )}
       ref={setNodeRef}
-      style={{ color: isOver ? "" : "" }}
+      // style={{ color: isOver ? "border border-2 border-blue-400" : "" }}
     >
-      <DndContext onDragEnd={reorderInputsList}>
-        <SortableContext
-          items={inputsList.map((input) => input.id)}
-          strategy={verticalListSortingStrategy}
-        >
-          {inputsList.map((input) => (
-            <MainCard
-              key={input.id}
-              inputData={input}
-              setInputsList={setInputsList}
-            />
-          ))}
-        </SortableContext>
-      </DndContext>
+      {/* <DndContext onDragEnd={reorderInputsList}> */}
+      <SortableContext
+        items={inputsList.map((input) => input.id)}
+        strategy={verticalListSortingStrategy}
+      >
+        {inputsList.map((input) => (
+          <MainCard
+            key={input.id}
+            inputData={input}
+            setInputsList={setInputsList}
+          />
+        ))}
+      </SortableContext>
+      {/* </DndContext> */}
     </div>
   );
 }

7d5b5476819702b3297ed29b5c69c2ad8d625184
Shivam77607 <shivamsharma77607@gmail.com>
2024-11-25 16:56:09 +0530

    refactor: initialize templates  with default values and update layout

--- components/LeftBar/index.tsx
+++ components/LeftBar/index.tsx
@@ -3,17 +3,40 @@
 import { useState } from "react";
 import LeftBarCard from "./LeftBarCard";
 import CreateTempDialog from "./CreateTempDialog";
-import { TemplateProps } from "@/types";
+import { InputType, TemplateProps } from "@/types";
 
 export default function LeftBar() {
-  const [templates, setTemplates] = useState<TemplateProps[]>([]);
+  const [templates, setTemplates] = useState<TemplateProps[]>([
+    {
+      id: "1",
+      name: "Text",
+      type: InputType.TEXT,
+      question: "Enter your email",
+      minLength: 5,
+      maxLength: 10,
+    },
+    {
+      id: "2",
+      name: "MCQ",
+      type: InputType.MCQ,
+      question: "Select your favourite color",
+      options: ["Red", "Green", "Blue"],
+    },
+  ]);
 
   return (
-    <div className="max-w-[20%] bg-slate-400 w-full flex flex-col gap-1 py-4 px-2">
+    <div className="max-w-[20%] bg-slate-600 w-full flex flex-col justify-between py-4 sticky top-0 left-0 h-screen max-h-screen">
+      <div className="flex flex-col justify-center gap-4">
+        <h1 className="text-2xl font-semibold text-left text-white mx-4">
+          Your Templates
+        </h1>
+        <div className="flex flex-col items-center">
+          {templates.map((template, index) => (
+            <LeftBarCard key={template.id} template={template} index={index} />
+          ))}
+        </div>
+      </div>
       <CreateTempDialog setTemplates={setTemplates} />
-      {templates.map((template) => (
-        <LeftBarCard key={template.id} template={template} />
-      ))}
     </div>
   );
 }

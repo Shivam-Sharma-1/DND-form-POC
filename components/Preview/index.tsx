f8dc929ec4dea3c24f5f7e53d93eb5d2e7346711
Shivam77607 <shivamsharma77607@gmail.com>
2024-11-26 18:55:00 +0530

    feat: enhance Preview component styling and layout for improved user experience

--- components/Preview/index.tsx
+++ components/Preview/index.tsx
@@ -5,12 +5,12 @@ import { Button } from "../UI/shadcn/button";
 const Preview = ({ inputsList }: { inputsList: TemplateProps[] }) => {
   return (
     <div className="flex flex-col items-center py-8">
-      <div className="bg-white max-w-[800px] w-full py-6 px-6 flex flex-col gap-4 rounded-md">
+      <div className="bg-white max-w-[800px] w-full py-10 px-8 flex flex-col gap-8 rounded-md shadow">
         {inputsList.map((input) => (
           <PreviewCard key={input.id} inputData={input} />
         ))}
         <div className="flex justify-end items-center">
-          <Button className="">Submit</Button>
+          <Button>Submit</Button>
         </div>
       </div>
     </div>

305a810f72e8c5e600b2d5822c8886c35029cd20
Shivam77607 <shivamsharma77607@gmail.com>
2024-11-26 16:01:45 +0530

    feat: add drag start handling and manage active element state in drag-and-drop

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@
 
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import { DndContext, DragEndEvent } from "@dnd-kit/core";
+import {
+  Active,
+  DndContext,
+  DragEndEvent,
+  DragStartEvent,
+} from "@dnd-kit/core";
 import LeftBar from "@/components/LeftBar";
 import Main from "@/components/Main";
 import { InputType, TemplateProps } from "@/types";
@@ -26,36 +31,7 @@ export default function Home() {
       options: ["Male", "Female", "Other"],
     },
   ]);
-
-  // const insertInput = (e: DragEndEvent) => {
-  //   if (!e.over) return;
-  //   console.log("event insert", e);
-
-  //   const newInput = e.active.data.current;
-  //   if (e.over?.id !== "droppable" || !newInput) {
-  //     return;
-  //   }
-
-  //   if (newInput && newInput.type && newInput.name && newInput.question) {
-  //     setInputsList((prevList) => [
-  //       ...prevList,
-  //       {
-  //         id: uuidv4(),
-  //         type: newInput.type,
-  //         name: newInput.name,
-  //         question: newInput.question,
-  //         ...(newInput.type === InputType.TEXT
-  //           ? { minLength: newInput.minLength, maxLength: newInput.maxLength }
-  //           : { minLength: undefined, maxLength: undefined }),
-  //         ...(newInput.type === InputType.MCQ
-  //           ? { options: newInput.options }
-  //           : { options: undefined }),
-  //       },
-  //     ]);
-  //   } else {
-  //     console.error("newInput is missing required properties", newInput);
-  //   }
-  // };
+  const [activeElement, setActiveElement] = useState<Active | null>(null);
 
   const handleDragEnd = (e: DragEndEvent) => {
     const { over, active } = e;
@@ -135,13 +111,18 @@ export default function Home() {
     }
   };
 
+  const handleDragStart = (e: DragStartEvent) => {
+    setActiveElement(e.active);
+  };
+
   return (
     <DndContext
+      onDragStart={handleDragStart}
       onDragEnd={handleDragEnd}
       // onDragOver={(e) => console.log("dragover", e)}
     >
       <main className="flex w-full min-h-screen">
-        <LeftBar />
+        <LeftBar activeElement={activeElement} />
         <div className="flex-1 bg-slate-200">
           <Main inputsList={inputsList} setInputsList={setInputsList} />
         </div>
